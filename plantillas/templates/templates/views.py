60a7618ff048dab24fca7a4b94d8187fda4c9f78
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-12-07 14:49:14 -0600

    07-12-22 EUM

---
+++ plantillas/templates/templates/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+#import datetime
+from django.template import Template, Context
+
+
+#USO DE PLANTILLAS
+
+def saludo(request):#vista se declara como una funcion
+    #saludo="""<html><body><h1>Hola mundito</h1></body></html>"""
+    doc_saludo=open("/home/eum/Documentos/Cursos/python/django/proyectos/django-learning/plantillas/templates/templates/plantillas_doc/plantilla1.html")
+    
+
+    plt=Template(doc_saludo.read())#objeto de tipo template
+    doc_saludo.close() #cierre del documento
+    #contexto (al momento vacio ya que no hay contenido dinamico)
+    ctx=Context()
+    documento=plt.render(ctx)
+    return HttpResponse(documento)
+
+
+#Uso de variables, objetos y propiedades en plantillas
